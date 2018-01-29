a23fa85e5c3c4894d3f1372e614544003a319dbc
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-01-29 11:03:51 -0500

    Changes to make the tests pass

--- sshfilter.py
+++ sshfilter.py
@@ -9,9 +9,11 @@ import re
 
 from config import Config
 
+
 def check_output(command):
     return subprocess.check_output(shlex.split(command),
                                    universal_newlines=True).splitlines()
+
 
 def get_country(ip):
     output = check_output('geoiplookup ' + ip)[0]
@@ -19,29 +21,33 @@ def get_country(ip):
     country = regex.findall(output)[0]
     return country
 
+
 def deny(ip):
     country = get_country(ip)
     return country in Config['COUNTRIES']['DENY']
+
 
 def allow(ip):
     country = get_country(ip)
     return country in Config['COUNTRIES']['ALLOW']
 
+
 def parse_arguments(args):
     parser = argparse.ArgumentParser(
-        description = 'ssh access filter')
+        description='ssh access filter')
     parser.add_argument("access", action='store',
-        choices=['allow', 'deny'], help='type of access')
+                        choices=['allow', 'deny'], help='type of access')
     parser.add_argument("ip", action='store', help='ip address')
 
     res = parser.parse_args(args)
     return res
-    
+
+
 def process_arguments(args):
     # print(args)
     # print(Config['COUNTRIES']['DENY'].split(' '))
     # print(Config['COUNTRIES']['ALLOW'].split(' '))
-    # 
+    #
     # for ip in ['218.87.109.152', '223.206.125.118', '8.8.8.8']:
     #     print(get_country(ip))
     #     print(deny(ip))
@@ -59,7 +65,7 @@ def process_arguments(args):
         else:
             sys.exit(1)
 
+
 if __name__ == "__main__":
     args = parse_arguments(sys.argv[1:])
     process_arguments(args)
-
