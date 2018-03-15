10f1a50858437ed3c88147298649f69f6cc3171f
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-03-15 12:39:00 -0400

    Update refused_countries.sh

--- supplementary/refused_countries.sh
+++ supplementary/refused_countries.sh
@@ -1,8 +1,9 @@
-outfile=bad_countries.txt_`date +'%Y%m%d_%H%M%S'`
-cat /var/log/auth.log | grep -Eo 'sshd\[[0-9]{1,}\]:.refused connect from ([0-9]{,3}\.?)*' | sed "s/^.*refused connect from //g" | uniq | {
+log_file=/var/log/auth.log
+out_file=bad_countries.txt_`date +'%Y%m%d_%H%M%S'`
+cat $log_file | grep -Eo 'sshd\[[0-9]{1,}\]:.refused connect from ([0-9]{,3}\.?)*' | sed "s/^.*refused connect from //g" | uniq | {
 while read r
 do
     geoiplookup $r
 done
-} | sort | uniq | cut -c 24-25 | sort | uniq | grep -vE "IN|US" > $outfile
-echo output stored in $outfile
+} | sort | uniq | cut -c 24-25 | sort | uniq | grep -vE "IN|US" > $out_file
+echo output stored in $out_file
