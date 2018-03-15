9e0a9335479c2a7d3770d0ea5ddf3246cc0b67b7
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-03-15 11:12:59 -0400

    Dump list of bad countries into a file

--- supplementary/refused_countries.sh
+++ supplementary/refused_countries.sh
@@ -1,6 +1,8 @@
+outfile=bad_countries.txt_`date +'%Y%m%d_%H%M%S'`
 cat /var/log/auth.log | grep -Eo 'sshd\[[0-9]{1,}\]:.refused connect from ([0-9]{,3}\.?)*' | sed "s/^.*refused connect from //g" | uniq | {
 while read r
 do
     geoiplookup $r
 done
-} | sort | uniq | cut -c 24-25 | sort | uniq
+} | sort | uniq | cut -c 24-25 | sort | uniq | grep -vE "IN|US" > $outfile
+echo output stored in $outfile
