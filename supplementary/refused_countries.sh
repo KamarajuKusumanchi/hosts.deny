521165cab88c8f9a4fa1aa1b46d458f96a0bb7c9
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-03-16 10:27:04 -0400

    Update refused_countries.sh

--- supplementary/refused_countries.sh
+++ supplementary/refused_countries.sh
@@ -1,5 +1,6 @@
-log_file=/var/log/auth.log
-out_file=bad_countries.txt_`date +'%Y%m%d_%H%M%S'`
+log_file=${1:-/var/log/auth.log}
+out_file=${2:-bad_countries.txt_`date +'%Y%m%d_%H%M%S'`}
+echo reading from $log_file
 cat $log_file | grep -Eo 'sshd\[[0-9]{1,}\]:.refused connect from ([0-9]{,3}\.?)*' | sed "s/^.*refused connect from //g" | uniq | {
 while read r
 do
