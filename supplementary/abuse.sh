006f5205b4bd4249b0871f79da2fbf315e03fdda
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-04-27 09:12:33 -0400

    Update abuse.sh

--- supplementary/abuse.sh
+++ supplementary/abuse.sh
@@ -1,10 +1,14 @@
 #! /usr/bin/env bash
 set -u
 set -e
-for i in `grep -v '^#' hosts.deny  | grep -v "/" | cut -f2 -d ' '`
+hosts_deny_file=${1:-~/work/github/hosts.deny/hosts.deny}
+out_file=${2:-junk.txt}
+echo reading from $hosts_deny_file
+for i in `grep -v '^#' $hosts_deny_file  | grep -v "/" | cut -f2 -d ' '`
 do
     echo $i
-    whois $i | grep -i abuse
+    whois $i | grep -Ei "abuse|range"
     sleep 1
     echo
 done > junk.txt 2>&1
+echo output stored in $out_file
