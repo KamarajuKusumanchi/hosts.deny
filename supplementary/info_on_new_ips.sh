0fd2bb368c7213b033825de4ac3b88c96ed4ec2a
Kamaraju S. Kusumanchi <kamaraju@gmail.com>
2018-06-25 10:07:50 -0400

    Add stuff

--- supplementary/info_on_new_ips.sh
+++ supplementary/info_on_new_ips.sh
@@ -9,7 +9,7 @@ cd $src_dir
 git diff hosts.deny | grep "^+" | grep -v '^++' | cut -f 2 -d ' ' | while read r
 do
   echo $r
-  geoiplookup -f ~/data/geoiplookup/GeoIP.dat $r
+  geoiplookup -f ~/.local/share/GeoIP/GeoIP.dat $r
   grep $r ~/x/ipsum.txt_RK_*
   echo
   whois $r | grep -Ei "abuse|range"
